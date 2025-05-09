8b3d388d72cc5e457ccd2c7a916f8426b6e54088
ocots <66357348+ocots@users.noreply.github.com>
2025-05-09 00:24:54 +0000

    :robot: Format .jl files

--- docs/make.jl
+++ docs/make.jl
@@ -9,9 +9,7 @@ makedocs(;
     format=Documenter.HTML(;
         repolink="https://" * repo_url,
         prettyurls=false,
-        size_threshold_ignore=[
-            ""
-        ],
+        size_threshold_ignore=[""],
         assets=[
             asset("https://control-toolbox.org/assets/css/documentation.css"),
             asset("https://control-toolbox.org/assets/js/documentation.js"),
